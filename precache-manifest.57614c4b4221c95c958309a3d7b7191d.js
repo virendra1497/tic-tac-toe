7b67d00f8c457a1cd3dd5950d53bdbfdcdfc7c2c
Virendra Parihar <virendra.parihar3025@gmail.com>
2020-09-29 14:20:46 +0530

    Create gh-pages branch with static content

---
+++ precache-manifest.57614c4b4221c95c958309a3d7b7191d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1a4874298bf79760fc7151151c10c36",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "0c2e1a6bad7eb5d9cfd5",
+    "url": "/tic-tac-toe/static/css/main.4e668706.chunk.css"
+  },
+  {
+    "revision": "0b592ed348ddc64f946e",
+    "url": "/tic-tac-toe/static/js/2.448192c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.448192c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c2e1a6bad7eb5d9cfd5",
+    "url": "/tic-tac-toe/static/js/main.4bdaf5ea.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  }
+]);
